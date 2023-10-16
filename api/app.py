adab931f444833c8557a24d58f4de2d279bd3168
Karla Dedeler <kld23@imperial.ac.uk>
2023-10-16 16:20:53 +0100

    added new random image function

--- api/app.py
+++ api/app.py
@@ -9,4 +9,5 @@ def hello_world():
 def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
-    return render_template("hello.html", name=input_name, age=input_age)
+    link = "https://picsum.photos/"+input_name+"/"+input_age
+    return render_template("hello.html", link=link)
