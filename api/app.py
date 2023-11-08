5aa0c3cd8716a09812080c69d499191e4c0cbe51
Joey Leung <joey.leung23@imperial.ac.uk>
2023-11-08 16:36:32 +0000

    fixed 403 error

--- api/app.py
+++ api/app.py
@@ -18,23 +18,29 @@ def submit():
     return render_template("hello.html", link=link)
 
 
-@app.route("/github", methods=["GET", "POST"])
-def github():
-    repos = None
+@app.route("/apis", methods=["GET", "POST"])
+def apis():
+    repos = [{}]
     username = request.form.get("username")
     if username:
         response = requests.get("https://api.github.com/users/"
                                 + username + "/repos")
-        print(response.status_code)
         if response.status_code == 200:
             repos = response.json()
             for repo in repos:
                 url_raw = requests.get(repo["commits_url"][:-6])
                 url = url_raw.json()
-                if url:
+                if url_raw.status_code == 200:
                     repo["newest_commit"] = url[0]["sha"]
                     repo["newest_commit_message"] = url[0]["commit"]["message"]
                     repo["num_commits"] = len(url)
+        elif response.status_code == 403:
+            repos[0]['full_name'] = ""
+            repos[0]['clone_url'] = "Too many requests, try again later!"
+            repos[0]['updated_at'] = ""
+            repos[0]['newest_commit'] = ""
+            repos[0]['newest_commit_message'] = ""
+            repos[0]['num_commits'] = ""
     else:
         response = "Not found"
         repos = None
